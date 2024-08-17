234ecac36b0f3dabad6984f44d5166f166ba2b15
Panashe Tapfumaneyi <99790286+PanasheTapfumaneyi@users.noreply.github.com>
2024-08-17 14:18:36 +0400

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors:{
+        darkGrey: '#181B1B',
+        lightGrey: '#2D3232',
+      }
+    },
+  },
+  plugins: [],
+}
+
