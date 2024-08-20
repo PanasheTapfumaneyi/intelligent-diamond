9b34227840acc04dc26c1e12ba414a2a3fe95497
Panashe Tapfumaneyi <99790286+PanasheTapfumaneyi@users.noreply.github.com>
2024-08-20 17:09:00 +0400

    Work on adding sections and content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./dist/*.{html,js}"],
   theme: {
     extend: {
       colors:{
         darkGrey: '#181B1B',
         lightGrey: '#2D3232',
-      }
+        darkGreen: '#02353C',
+        textGreen: '#07FF9F'
+      },
+      fontFamily: {
+        abeezee: ['ABeeZee', 'sans-serif'],
+        bangers: ['Bangers', 'cursive'],
+        fredoka: ['Fredoka', 'sans-serif'],
+      },
+      fontSize: {
+        'header-font': '3.815rem',
+        'paragraph-font': '1.25rem'
+      },
+      letterSpacing: {
+        'custom-tight': '-0.05em',
+        'custom-wide': '0.2em',
+      },
     },
   },
   plugins: [],
